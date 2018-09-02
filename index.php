8ba87452268b9bff2c2d4b41c6c51a36e842bc9d
Vyacheslav <viacheslav.suprun@yandex.ru>
2018-09-02 13:34:26 +0300

    php-forms

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+echo "<strong>_POST array:</strong><br>";
+print_r ($_POST);
+echo "<br><br><br>";
+
+if (!empty($_POST)) {
+    
+    $message = " Вам пришло новое сообщение с сайта:\n" 
+    . "Имя отправителя: " . $_POST['userName'] . "\n" 
+    . "Email отправителя: " . $_POST['userEmail'] . "\n"
+    . "Сообщение отправителя: \n " . $_POST['message']; 
+    
+    $headers = "From: info@webcademy.ru";
+    
+    $resultMail = mail( "viacheslav.suprun@yandex.ru", "Сообщение с сайта", $message, $headers );
+    
+    if ( $resultMail ) {
+        echo "Форма отправлена успешно!";
+    } else {
+        echo "Что то пошло не так. Форма не отправлена.";
+    }
+}
+
+?>
+
+<form action="index.php" method="post">
+    <input type="text" name="userName" placeholder="Ваше имя"><br>
+    <input type="text" name="userEmail" placeholder="Ваш Email"><br>
+    <textarea name="message" id="" cols="30" rows="10" placeholder="Сообщение"></textarea><br>
+    <input type="submit" value="Отправить форму!"><br>
+</form>
